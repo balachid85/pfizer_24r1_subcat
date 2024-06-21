c58ab46a8049a7d7e81a39a77e0bc19199ba0c4b
Velvizhi J <101398359+Velvizhi22@users.noreply.github.com>
2024-06-21 17:05:01 +0530

    converted SAAS Code

--- model_libs/AECM1_1.py
+++ model_libs/AECM1_1.py
@@ -13,96 +13,121 @@ except:
     from base import BaseSDQApi
     import utils
 
-import traceback, tqdm, logging, numpy as np, yaml, os
-from datetime import datetime
-curr_file_path = os.path.realpath(__file__)
-curr_path = os.path.abspath(os.path.join(curr_file_path, '../'))
-subcat_config_path = os.path.join(curr_path, 'subcate_config.yml')
-a = yaml.safe_load(open(subcat_config_path, 'r'))
+import traceback, tqdm, numpy as np, logging, yaml, os
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 class AECM1_1(BaseSDQApi):
-    domain_list = [
-     'AE', 'CM']
+    def __init__(self, study_id, account_id, job_id, rule_id, version, thread_id=None, thread_batch_count = Config.THREAD_BATCH_COUNT, subjects=None):        
+        try:
+            super().__init__(study_id, account_id, job_id, rule_id, version, thread_id=thread_id, thread_batch_count = thread_batch_count, subjects=subjects)
+            self.sub_cat = self.__class__.__name__ #'AECM0'
+            self.fn_config = self.get_field_dict(self.account_id, self.study_id, self.sub_cat, 'fn_config')
+            self.domain_list = self.fn_config['domain_list']    
+        except Exception as init_exc:
+            print('Exception while getting config values in init :', init_exc)
+            print(traceback.format_exc())
 
+    
     def execute(self):
         study = self.study_id
-        sub_cat = 'AECM1_1'
-        subjects = self.get_subjects(study, domain_list = self.domain_list, per_page = 10000)
+        sub_cat = self.sub_cat
+        fn_config = self.fn_config
+        f_d = 'display_fields'
+        
+        try:  
+            fields_dict = (self.get_field_dict(self.account_id, self.study_id, sub_cat, f_d))
+            fields_labels_1 = self.get_field_labels(self.account_id,self.domain_list[0])
+            fields_labels_2 = self.get_field_labels(self.account_id,self.domain_list[1])
+            fields_labels = {**fields_labels_1, **fields_labels_2}
+            
+            match_terms = fn_config['match']
+            match_cond = fn_config['match_condition']
+            cols = fn_config['cols']
+            vals = fn_config['vals']
+            smc = fn_config['use_smc'] if 'use_smc' in fn_config.keys() else False
+            predict_med_code = None
+            subjects = self.get_subjects(study, domain_list=self.domain_list, per_page = 10000)
+        except Exception as fn_exc:
+            print('Exception while getting config values / subjects :', fn_exc)
+            print(traceback.format_exc())             
+            
         for subject in tqdm.tqdm(subjects):
+            print('The Subject is :', subject)
             payload_list = []
             try:
                 flatten_data = self.get_flatten_data(study, subject, per_page=10000, domain_list=(self.domain_list))
                 if not flatten_data.get(self.domain_list[0],[]):
                     if self.has_auto_closure(self.study_id,  self.rule_id, self.version):
                         self.close_query(subject, payload_list)
-                ae_df = pd.DataFrame(flatten_data['AE'])
-                cm_df = pd.DataFrame(flatten_data['CM'])
+                ae_df = pd.DataFrame(flatten_data.get(self.domain_list[0],[]))
+                cm_df = pd.DataFrame(flatten_data.get(self.domain_list[1],[]))
+                # ae_df = pd.DataFrame(flatten_data['AE'])
+                # cm_df = pd.DataFrame(flatten_data['CM'])
                 for ind in range(ae_df.shape[0]):
                     try:
                         ae_record = ae_df.iloc[[ind]]
-                        formindex = ae_record['form_index'].values[0]
-                        o_aespid = ae_record['AESPID'].values[0]
-                        ae_cm_records = utils.extractor(ae_record, ae_df, cm_df, 'AESPID', 'CMAENO')
+                        formindex = ae_record[cols['form_index_col'][0]].values[0]
+                        o_aespid = ae_record[cols['aespid_col'][0]].values[0]
+                        ae_cm_records = utils.extractor(ae_record, ae_df, cm_df, cols['aespid_col'][0], cols['cmaeno_col'][0])
                         if (type(ae_cm_records) == tuple) & (type(ae_cm_records) != bool): 
                             if len(ae_cm_records[0]) > 0:      
                                 for ae_formindex, cm_records in ae_cm_records[0].items():
                                         for cm_record in cm_records:
-                                            new_ae_records, new_cm_record = utils.get_cm_hierarchy(ae_record, ae_df, cm_record, key = 'AESTDAT')
-                                            for aeptcd in new_ae_records['AEPTCD'].unique().tolist():
-                                                new_ae_record = new_ae_records[new_ae_records['AEPTCD'] == aeptcd]
+                                            new_ae_records, new_cm_record = utils.get_cm_hierarchy(ae_record, ae_df, cm_record, key = cols['aestdtc_dts_col'][0])
+                                            for aeptcd in new_ae_records[cols['aeptcd_col'][0]].unique().tolist():
+                                                new_ae_record = new_ae_records[new_ae_records[cols['aeptcd_col'][0]] == aeptcd]
                                                 if len(new_ae_record) > 1:
                                                     new_ae_record = new_ae_record.head(1)
-                                                aespid = new_ae_record['AESPID'].values[0]
+                                                aespid = new_ae_record[cols['aespid_col'][0]].values[0]
                                                 new_ae_record1 = new_ae_record
                                                 if aespid != o_aespid:
                                                     continue
                                                 if type(new_ae_record) != int:
                                                     dtr_null_flag=False
-                                                    if 'AESTDAT_DTR' in new_ae_record.columns:
-                                                        if new_ae_record['AESTDAT_DTR'].astype('str').values[0] in utils.null_list: 
+                                                    if cols['aestdat_dtr_col'][0] in new_ae_record.columns:
+                                                        if new_ae_record[cols['aestdat_dtr_col'][0]].astype('str').values[0] in utils.null_list: 
                                                             dtr_null_flag=True
-                                                    if 'AESTDAT_DTR' in new_ae_record.columns and not dtr_null_flag:
-                                                        new_ae_record['AESTDAT_DTR'] = new_ae_record['AESTDAT_DTR'].astype('str')
-                                                        ae_stdt = new_ae_record['AESTDAT_DTR'].values[0]
+                                                    if cols['aestdat_dtr_col'][0] in new_ae_record.columns and not dtr_null_flag:
+                                                        new_ae_record[cols['aestdat_dtr_col'][0]] = new_ae_record[cols['aestdat_dtr_col'][0]].astype('str')
+                                                        ae_stdt = new_ae_record[cols['aestdat_dtr_col'][0]].values[0]
                                                     else:
-                                                        new_ae_record['AESTDAT'] = new_ae_record['AESTDAT'].astype('str')
-                                                        ae_stdt = new_ae_record['AESTDAT'].values[0]
-                                                    new_cm_record['CMSTDAT_DTR'] = new_cm_record['CMSTDAT_DTR'].astype('str')
-                                                    cm_stdt = new_cm_record['CMSTDAT_DTR'].values[0]
+                                                        new_ae_record[cols['aestdtc_dts_col'][0]] = new_ae_record[cols['aestdtc_dts_col'][0]].astype('str')
+                                                        ae_stdt = new_ae_record[cols['aestdtc_dts_col'][0]].values[0]
+                                                    new_cm_record[cols['cmstdat_dtr_col'][0]] = new_cm_record[cols['cmstdat_dtr_col'][0]].astype('str')
+                                                    cm_stdt = new_cm_record[cols['cmstdat_dtr_col'][0]].values[0]
                                                     unk_flag, year_not_unk, date_dict = utils.impute_unk({'aestdat':ae_stdt,  'cmstdat':cm_stdt})
                                                     if not year_not_unk:
                                                         continue
                                                     
                                                     aestdat = date_dict['aestdat'].date()
                                                     cmstdat = date_dict['cmstdat'].date()
-                                                    aecmgiv = new_ae_record['AECMGIV'].values[0]
+                                                    aecmgiv = new_ae_record[cols['aecmgiv_col'][0]].values[0]
                                                     if cmstdat < aestdat and aecmgiv.upper() == 'YES':   
                                                         subcate_report_dict = {}
                                                         report_dict = {}
-                                                        new_cm_record['CMAENO'] = cm_record['CMAENO']
+                                                        new_cm_record[cols['cmaeno_col'][0]] = cm_record[cols['cmaeno_col'][0]]
                                                         new_ae_record1,new_cm_record = utils.unk_format_datetime(unk_flag, new_ae_record1, new_cm_record)
                                                         piv_rec = {'AE' : new_ae_record1.head(1), 'CM' : new_cm_record.head(1)}
-                                                        for dom, cols in a['FIELDS_FOR_UI'][sub_cat].items():
+                                                        for dom, columns in fields_dict.items():
                                                             piv_df = piv_rec[dom]
-                                                            present_col = [col for col in cols if col in piv_df.columns.tolist()]
+                                                            present_col = [col for col in columns if col in piv_df.columns.tolist()]
                                                             rep_df = piv_df[present_col]
-                                                            rep_df['deeplink'] = utils.get_deeplink(study, piv_df)
-                                                            rep_df = rep_df.rename(columns = a['FIELD_NAME_DICT'])
+                                                            try:
+                                                                rep_df['deeplink'] = utils.get_deeplink(study, piv_df, self.get_study_source(), subject_id=self.get_map_subject_id(piv_df['subj_guid'].values[0]))
+                                                            except:
+                                                                rep_df['deeplink'] = '#'
+                                                            rep_df = rep_df.rename(columns=fields_labels)
+                                                            rep_df = rep_df.loc[:,~rep_df.columns.duplicated()]                                    
                                                             report_dict[dom]= rep_df.to_json(orient= 'records')
-
                                                         subcate_report_dict[sub_cat] =  report_dict
-                                                        aeterm = new_ae_record['AETERM'].values[0]
-                                                        formidx = new_cm_record['form_ix'].values[0]
-                                                        cmtrt = new_cm_record['CMTRT'].values[0]
-                                                        ae_stdt = new_ae_record1['AESTDAT'].values[0]
-                                                        cm_stdt = new_cm_record['CMSTDAT'].values[0]
+                                                    
                                                         ae_record1 = ae_record.iloc[0]
-                                                        query_text_param = {'CM_FORMIDX':formidx, 
-                                                        'CMTRT':cmtrt, 
-                                                        'AESPID':aespid, 
-                                                        'AETERM':aeterm, 
-                                                        'CMSTDAT':cm_stdt, 
-                                                        'AESTDAT':new_ae_record1['AESTDAT'].values[0]}
+                                                        query_text_param = {}
+                                                        try:
+                                                            query_text_param = utils.get_qt_payload(fn_config, piv_rec)
+                                                        except:
+                                                            print('QT Error: ',traceback.format_exc())  
                                                         payload = {'subcategory':sub_cat,
                                                         'cdr_skey':str(ae_record1['cdr_skey']),
                                                         'query_text':self.get_model_query_text_json(study, sub_cat, params=query_text_param), 
@@ -113,6 +138,7 @@ class AECM1_1(BaseSDQApi):
                                                         'formrefname':str(ae_record1['formrefname']), 
                                                         'report':subcate_report_dict, 
                                                         'confid_score':np.random.uniform(0.7, 0.97)}
+                                                        print('Subject and Payload are :',subject,payload)
                                                         self.insert_query(study, subject, payload)
 
                                                     if payload not in payload_list:
